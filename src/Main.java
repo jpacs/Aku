b42cccde541d51f1e25bc8f9c5a1255e96880e8a
Joao Santos <jps2pt@gmail.com>
2012-06-28 15:11:36 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+			try {
+				Display.create();
+			} catch (LWJGLException e) {
+				e.printStackTrace();
+			}
+	}
+
+}
